4ae120c428496be164febaf5fa59b0baaa34a3b0
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-10 12:44:53 +0400

    Refactor Customer class annotations and imports

--- src/main/java/com/example/Task10Demo/Entity/Customer.java
+++ src/main/java/com/example/Task10Demo/Entity/Customer.java
@@ -2,12 +2,14 @@ package com.example.Task10Demo.Entity;
 
 import jakarta.el.ELManager;
 import jakarta.persistence.Entity;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.Table;
+import lombok.*;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-@Getter
-@Setter
+@Table
 public class Customer extends BaseEntity {
     private String customerName;
     private String accountNumber;
