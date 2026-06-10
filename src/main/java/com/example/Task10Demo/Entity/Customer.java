875a4192218f790afcd9a6ae313ce1b0556b61e9
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-10 15:27:45 +0400

    Add validation annotations to Customer fields

--- src/main/java/com/example/Task10Demo/Entity/Customer.java
+++ src/main/java/com/example/Task10Demo/Entity/Customer.java
@@ -1,8 +1,13 @@
 package com.example.Task10Demo.Entity;
 
 import jakarta.el.ELManager;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.*;
 
 @Data
@@ -11,9 +16,13 @@ import lombok.*;
 @Entity
 @Table
 public class Customer extends BaseEntity {
+    @NotBlank(message = "Customer Name Cannot Be Empty!")
     private String customerName;
     private String accountNumber;
+    @PositiveOrZero(message = "Balance Cannot Be Negative!")
     private Double balance;
+    @Email(message = "Email must be valid!")
+    @NotBlank(message = "Email Cannot Be Empty!")
     private String email;
     private String phoneNumber;
 
