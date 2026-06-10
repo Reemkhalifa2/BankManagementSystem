483b8463c5ef6b1dc1eea9ef67ed0e1298dbc879
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-10 10:39:44 +0400

    Create base Entity

---
+++ src/main/java/com/example/Task10Demo/Entity/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.example.Task10Demo.Entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+
+@MappedSuperclass
+@Getter
+@Setter
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private Date ctreationDate;
+    private Date updateDate;
+    private Boolean isActive;
+
+}
